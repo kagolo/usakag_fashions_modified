ba40f9173af8deb0dd7bc366990ce05a0a18c90f
kagolo <84382333+kagolo@users.noreply.github.com>
2022-09-28 05:40:20 -0700

    Added static root

--- usakag_fashions_modified/urls.py
+++ usakag_fashions_modified/urls.py
@@ -30,3 +30,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
