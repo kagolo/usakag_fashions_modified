e2c93dbd08efedbde85315d250a13688ad526123
kagolo <84382333+kagolo@users.noreply.github.com>
2022-09-28 00:34:00 -0700

    Changed models.py

--- usakag_fashions_modified/wsgi.py
+++ usakag_fashions_modified/wsgi.py
@@ -6,15 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-import os, sys
-# add the hellodjango project path into the sys.path
-sys.path.append('<PATH_TO_MY_DJANGO_PROJECT>/usakag_fashions_modified')
 
-# add the virtualenv site-packages path to the sys.path
-sys.path.append('<PATH_TO_VIRTUALENV>/Lib/site-packages')
-
-# poiting to the project settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "usakag_fashions_modified.settings")
+import os
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usakag_fashions_modified.settings')
+
 application = get_wsgi_application()
