b5903ddcd89c0bb28c9bcca34fca52a2dcffd0c3
kagolo <84382333+kagolo@users.noreply.github.com>
2022-09-27 21:53:35 -0700

    Added wsgi handlers

--- usakag_fashions_modified/wsgi.py
+++ usakag_fashions_modified/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
 import os
+import django
+from django.core.handlers.wsgi import WSGIHandler
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usakag_fashions_modified.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "usakag_fashions_modified.settings.production")
+django.setup(set_prefix=False)
 
-application = get_wsgi_application()
+application = WSGIHandler()
